b5715a8a73165e59b8e4787896bb995b140804f4
chinmoypborah <68630616+chinmoypborah@users.noreply.github.com>
2021-07-31 13:35:18 +0530

    Update bc.py

--- bc.py
+++ bc.py
@@ -5,43 +5,46 @@ ap = argparse.ArgumentParser()
 ap.add_argument('-i', '--image', required=True, help="Image Path")
 args = vars(ap.parse_args())
 img_path = args['image']
+if img_path.endswith(('.jpg','.jpeg')): #Checks for valid jpeg or jpg image.
 
-print("models are loading...")
-net = cv2.dnn.readNetFromCaffe('colorization_deploy_v2.prototxt','colorization_release_v2.caffemodel')
-pts = np.load('pts_in_hull.npy')
-
-
-class8 = net.getLayerId("class8_ab")
-conv8 = net.getLayerId("conv8_313_rh")
-pts = pts.transpose().reshape(2,313,1,1)
-
-net.getLayer(class8).blobs = [pts.astype("float32")]
-net.getLayer(conv8).blobs = [np.full([1,313],2.606,dtype='float32')]
+    print("models are loading...")
+    net = cv2.dnn.readNetFromCaffe('colorization_deploy_v2.prototxt','colorization_release_v2.caffemodel')
+    pts = np.load('pts_in_hull.npy')
 
 
-image = cv2.imread(img_path)
-scaled = image.astype("float32")/255.0
-lab = cv2.cvtColor(scaled,cv2.COLOR_BGR2LAB)
+    class8 = net.getLayerId("class8_ab")
+    conv8 = net.getLayerId("conv8_313_rh")
+    pts = pts.transpose().reshape(2,313,1,1)
+
+    net.getLayer(class8).blobs = [pts.astype("float32")]
+    net.getLayer(conv8).blobs = [np.full([1,313],2.606,dtype='float32')]
 
 
-resized = cv2.resize(lab,(224,224))
-L = cv2.split(resized)[0]
-L -= 50
+    image = cv2.imread(img_path)
+    scaled = image.astype("float32")/255.0
+    lab = cv2.cvtColor(scaled,cv2.COLOR_BGR2LAB)
 
 
-net.setInput(cv2.dnn.blobFromImage(L))
-ab = net.forward()[0, :, :, :].transpose((1,2,0))
+    resized = cv2.resize(lab,(224,224))
+    L = cv2.split(resized)[0]
+    L -= 50
 
-ab = cv2.resize(ab, (image.shape[1],image.shape[0]))
 
-L = cv2.split(lab)[0]
-colorized = np.concatenate((L[:,:,np.newaxis], ab), axis=2)
+    net.setInput(cv2.dnn.blobFromImage(L))
+    ab = net.forward()[0, :, :, :].transpose((1,2,0))
 
-colorized = cv2.cvtColor(colorized,cv2.COLOR_LAB2BGR)
-colorized = np.clip(colorized,0,1)
+    ab = cv2.resize(ab, (image.shape[1],image.shape[0]))
 
-colorized = (255 * colorized).astype("uint8")
+    L = cv2.split(lab)[0]
+    colorized = np.concatenate((L[:,:,np.newaxis], ab), axis=2)
 
-cv2.imshow("Original",image)
-cv2.imshow("Colorized",colorized)
-cv2.waitKey(0)
+    colorized = cv2.cvtColor(colorized,cv2.COLOR_LAB2BGR)
+    colorized = np.clip(colorized,0,1)
+
+    colorized = (255 * colorized).astype("uint8")
+
+    cv2.imshow("Original",image)
+    cv2.imshow("Colorized",colorized)
+    cv2.waitKey(0)
+else:
+    print("Invalid Image")
